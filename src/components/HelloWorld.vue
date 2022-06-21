7537020439d95c370832bfa505ee647d6c9b2924
Jakub Szymański <jszymanski.dev@gmail.com>
2022-06-21 12:19:14 +0200

    Add support for pre-processor SCSS

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -40,13 +40,17 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="scss" scoped>
 h3 {
   margin: 40px 0 0;
 }
 ul {
   list-style-type: none;
   padding: 0;
+
+  li {
+    border: 1px dashed #222;
+  }
 }
 li {
   display: inline-block;
